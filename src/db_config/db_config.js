02751bee7b0339b993a368ec944ff85df2972f8b
Mohammed Soharab S <soharab@quinta.co.in>
2023-07-10 08:23:02 +0530

    Data import

--- src/db_config/db_config.js
+++ src/db_config/db_config.js
@@ -1,12 +1,29 @@
 // db_config/db_config.js
-const mysql = require('mysql2');
+const mysql = require("mysql2");
+const fs = require("fs");
+const csv = require("csv-parser");
 
 const connection = mysql.createConnection({
-  host: 'localhost', // Hosted on GCP, using a VM and hence db is also at local
+  host: "localhost", // Hosted on GCP, using a VM and hence db is also at local
   port: 3306, // MySQL port
-  user: 'root', // Using root
-  password: 'password', // Replace with your MySQL password
-  database: 'booking_db', // Replace with your MySQL database name
+  user: "root", // Using root
+  password: "flurn_mysql", // Replace with your MySQL password
+  database: "booking_db", // Replace with your MySQL database name
 });
+
+// Read the CSV file
+fs.createReadStream("/var/lib/mysql-files/Seats.csv")
+  .pipe(csv({ separator: "\t", enclosed: '"' }))
+  .on("data", (row) => {
+    // Insert each row into the seats table
+    connection.query(
+      "INSERT INTO seats (seat_identifier, seat_class) VALUES (?, ?)",
+      [row.seat_identifier, row.seat_class]
+    );
+  })
+  .on("end", () => {
+    console.log("Data imported successfully");
+    connection.end();
+  });
 
 module.exports = connection;
