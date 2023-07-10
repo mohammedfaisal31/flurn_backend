90bf1fd36fb171691d6d84bbe7c4bee94f618b60
Mohammed Soharab S <soharab@quinta.co.in>
2023-07-11 01:25:23 +0530

    modified route /bookings

--- src/db_config/db_config.js
+++ src/db_config/db_config.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
   host: "localhost", // Hosted on GCP, using a VM and hence db is also at local
   port: 3306, // MySQL port
   user: "root", // Using root
-  password: process.env.DB_PASSWORD, // MySQL password stored in env
+  password: "flurn_mysql", // MySQL password stored in env
   database: "booking_db", // MySQL database name
 });
 
