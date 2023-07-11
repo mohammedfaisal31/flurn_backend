5c365585986a7e543e93f0eb4c834abe85e08808
Mohammed Soharab S <soharab@quinta.co.in>
2023-07-11 08:43:57 +0530

    modified route /bookings

--- src/models/bookingModel.js
+++ src/models/bookingModel.js
@@ -17,33 +17,34 @@ async function getSeatPricing(id) {
     const [rows] = await db.promise().query(
       `
       SELECT
-      s.id,
-      s.seat_identifier,
-      s.seat_class,
-      CASE
-        WHEN s.booked_count < s.total_seats * 0.4 THEN IFNULL(sp.min_price, sp.normal_price)
-        WHEN s.booked_count >= s.total_seats * 0.4 AND s.booked_count <= s.total_seats * 0.6 THEN IFNULL(sp.normal_price, sp.max_price)
-        WHEN s.booked_count > s.total_seats * 0.6 THEN IFNULL(sp.max_price, sp.normal_price)
-      END AS price
+  s.id,
+  s.seat_identifier,
+  s.seat_class,
+  CASE
+    WHEN s_count.booked_count < s_count.total_seats * 0.4 THEN IFNULL(sp.min_price, sp.normal_price)
+    WHEN s_count.booked_count >= s_count.total_seats * 0.4 AND s_count.booked_count <= s_count.total_seats * 0.6 THEN IFNULL(sp.normal_price, sp.max_price)
+    WHEN s_count.booked_count > s_count.total_seats * 0.6 THEN IFNULL(sp.max_price, sp.normal_price)
+  END AS price
+FROM
+  seats s
+JOIN
+  (
+    SELECT
+      seat_class,
+      COUNT(*) AS booked_count,
+      (SELECT COUNT(*) FROM seats s2 WHERE s2.seat_class = s.seat_class) AS total_seats
     FROM
       seats s
-    JOIN
-      (
-        SELECT
-          seat_class,
-          COUNT(*) AS booked_count,
-          (SELECT COUNT(*) FROM seats WHERE seat_class = s2.seat_class) AS total_seats
-        FROM
-          seats s2
-        WHERE
-          s2.is_booked = 1
-        GROUP BY
-          seat_class
-      ) AS s_count ON s.seat_class = s_count.seat_class
-    JOIN
-      seat_pricing sp ON s.seat_class = sp.seat_class
     WHERE
-      s.id = ?;
+      s.is_booked = 1
+    GROUP BY
+      seat_class
+  ) AS s_count ON s.seat_class = s_count.seat_class
+JOIN
+  seat_pricing sp ON s.seat_class = sp.seat_class
+WHERE
+  s.id = ?;
+
     `,
       [id]
     );
